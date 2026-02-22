e99843bd182c42ebb7df055e103767284d8e5010
Hasindu Chanuka <148543372+hasindu1998@users.noreply.github.com>
2026-02-22 21:01:59 +0530

    Update app.js

--- App.js
+++ App.js
@@ -3,15 +3,17 @@ console.log("Hasindu Chanuka");
 const condition = true;
 
 const myPromise = new Promise((resolve, reject) => {
-  if (condition) {
-    resolve('Success!');
-  } else {
-    reject('Failure!');
-  }
+ if (condition) resolve('Success!');
+  else reject('Failure!');
 });
 
-myPromise.then((result) => {
-  console.log(result);
-}).catch((error) => {
-  console.log(error);
-});
+async function myFunction() {
+  try {
+    const result = await myPromise;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+myFunction();
